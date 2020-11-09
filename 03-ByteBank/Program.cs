fc2f813c079e76c5dac2855acfbfb1e92eb2e044
Jessica Magalhaes <jfdmagalhaes@hotmail.com>
2020-11-09 18:35:34 -0300

    Tipo referencia

---
+++ 03-ByteBank/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contadaGabriela = new ContaCorrente();
+            contadaGabriela.titular = "Gabriela";
+            contadaGabriela.numeroAgencia = 123;
+            contadaGabriela.numero = 45678;
+
+
+            ContaCorrente contadaGabrielaCosta = new ContaCorrente();
+            contadaGabriela.titular = "Gabriela";
+            contadaGabriela.numeroAgencia = 123;
+            contadaGabriela.numero = 45678;
+
+            Console.WriteLine("Igualdade de tipo de referencia: " + (contadaGabriela == contadaGabrielaCosta));
+
+            Console.ReadLine();
+        }
+    }
+}
