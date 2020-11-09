59decf1e6badfcd4d78f036692f29ca5470d436b
Jessica Magalhaes <jfdmagalhaes@hotmail.com>
2020-11-09 18:03:19 -0300

    projeto da aula 2

---
+++ 01-ByteBank/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contadaGabriela = new ContaCorrente();
+
+            contadaGabriela.titular = "Gabriela";
+
+            Console.WriteLine(contadaGabriela.titular);
+
+            Console.ReadLine();
+
+        }
+    }
+}
